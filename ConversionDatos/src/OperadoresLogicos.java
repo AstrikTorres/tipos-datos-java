964fd1e713d6fbe4c9d9c7a160f8bba04bbb3a29
Astrik Torres <astriktorres@gmail.com>
2021-07-04 09:20:28 -0500

    Operadores lógicos en Java

---
+++ ConversionDatos/src/OperadoresLogicos.java
@@ -0,0 +1,26 @@
+
+public class OperadoresLogicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Operadores Lógicos
+		 * a == b: a es igual a b
+		 * a > b: a es mayor que b
+		 * a < b: a es menor que
+		 * a != b: a no es diferente a b
+		 * 
+		 * !
+		 * |
+		 * &
+		 * ||
+		 * &&
+		 */
+		System.out.println(saludo());
+	}
+
+	public static String saludo() {
+		return "saludo";
+	}
+	
+}
